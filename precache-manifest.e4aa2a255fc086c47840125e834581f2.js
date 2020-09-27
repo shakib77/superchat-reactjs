72de6f06590db1c1ca92a0d0f7e099858ed5a728
shakib77 <shakibshaon77@gmail.com>
2020-09-27 20:42:56 +0600

    Updates

---
+++ precache-manifest.e4aa2a255fc086c47840125e834581f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d71fe6d40d7adee122553a4715cc8b",
+    "url": "/superchat-reactjs/index.html"
+  },
+  {
+    "revision": "2b6517cacaa4a88efd62",
+    "url": "/superchat-reactjs/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "f61098a466f83c9096ca",
+    "url": "/superchat-reactjs/static/js/2.3d22cc8a.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/superchat-reactjs/static/js/2.3d22cc8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6517cacaa4a88efd62",
+    "url": "/superchat-reactjs/static/js/main.90122f30.chunk.js"
+  },
+  {
+    "revision": "547dddeda07e3679a72e",
+    "url": "/superchat-reactjs/static/js/runtime-main.22db2f49.js"
+  }
+]);
